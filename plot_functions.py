be078bbf4bab869b62f7d2db678db9865b0a00b8
kvn <kevin.galassi2@unibo.it>
2022-04-29 11:43:10 +0200

    dataset update

--- plot_functions.py
+++ plot_functions.py
@@ -10,6 +10,7 @@ def csv_elaboration(phase_types, results_types,data_types, base_path ) :
    force_df = pd.DataFrame()
    pos_df = pd.DataFrame()
 
+   test_counter = 0
    for phase in phase_types :
       for result in results_types :
          for data_type in data_types :
@@ -20,7 +21,9 @@ def csv_elaboration(phase_types, results_types,data_types, base_path ) :
                for i, file in enumerate(l) :
                   new_df = pd.read_csv(file)
                   new_df["Time"] = new_df["Time"].subtract(new_df['Time'][0])
-                  new_df['test'] = i
+                  new_df['test'] = test_counter
+                  test_counter += 1
+                  print(test_counter)
                   new_df['phase'] = phase
                   new_df['result'] = result
                   if data_type == 'force' :
@@ -28,4 +31,6 @@ def csv_elaboration(phase_types, results_types,data_types, base_path ) :
                   if data_type == 'position' :
                      pos_df = pd.concat([pos_df, new_df], ignore_index=True)
 
-   return force_df, pos_df
+   print('{} test loaded'.format(test_counter))
+
+   return force_df, pos_df, test_counter
